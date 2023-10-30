46e14e1b774cc35c5647f2b05318523063424406
Bigboss <ochie@Bigboss>
2023-10-30 15:50:44 +0300

    first python code

---
+++ introduction.b.py
@@ -0,0 +1,61 @@
+# Datatypes in python
+
+# Numerals
+num =24 # integer (int)
+pie=3.14 # float
+#double= 3.1424628472492
+
+# End of Numerals
+
+# String
+Wish="Happy birthday Uhuru Kenyatta"
+age ="19"
+num2="24" # string (str)''
+# End of String
+
+
+# Collection of items --arrays
+# list, tuple, range
+# list
+crushies=["Marian","Winnie","Murife","Embarambamba",254,[1,2,3]] # square brackets
+# Accessing items in the list
+# print(crushies[1])
+# print("Hi", crushies[1],"will you come over?")
+
+# print(crushies[3])
+# print("Hi,"crushies[3],"will you come over?")
+
+# print(crushies[5][1])
+array=[] # index/indexing
+
+
+# mutables - can alter
+#print(crushies)
+
+# tuple
+() # parenthesis
+mombasa =("beach boys,wababaz",254)
+#print("leo nataka"mombasa[1])
+
+# immutable- cannot be altered
+
+# End of arrays
+
+
+# Dictionary
+countries = {;
+   "Africa":["kenya", "pwani,"]
+     "Europe" : "germany"
+"crushies,": ["mariam","winnie", "murife", "Embarambamba"]
+1:"kilifi"
+ } # key - value
+
+
+print(countries)
+
+print(num, num2)
+
+name ="kilifi"
+
+
+
