bf9d0331bf5ac27435e2fad36c4036b9b7fe072e
Bigboss <ochie@Bigboss>
2023-11-08 08:39:55 +0300

    introduction to python

---
+++ introduction.a.py
@@ -0,0 +1,20 @@
+# To save, click Ctrl + s
+# To comment, press ctrl + /
+
+# Variables
+number=10
+a, b, c =4,5.5,"Hellow Kilifi!"
+print(number)
+print("")
+print(a, b, c)
+
+# first_name ="Kilifi"
+
+# _firstName ="Goa"
+
+# print(_firstName)
+
+num =5
+Num =55
+
+print(num, Num)
