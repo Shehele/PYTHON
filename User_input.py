a6a7c69da83e717f2cbf4f6d3a2b28a7c02f8f11
Bigboss <ochie@Bigboss>
2023-11-10 23:26:27 +0300

    code challenge

---
+++ User_input.py
@@ -0,0 +1,9 @@
+#Define variable to store the input
+
+name=input("Please enter your name:")
+
+age=int(input("Please enter your age:"))
+
+location=input("please enter your location:")
+#print the input
+print("Hellow," + name + "your" + str(age) +"rd" + "birthday party will be celebrated in" + location + "thanks and Happy birthday in advance")
---
+++ dict.py
@@ -0,0 +1,3 @@
+my_dict={"a": 1, "b": 2, "c": 3}
+result=my_dict["b"]
+print(result)
