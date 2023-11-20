7a06e9e16b8ec7fd6186ee96558f361c3403f27a
Bigboss <ochie@Bigboss>
2023-11-20 19:05:34 +0300

    conditionals

---
+++ Conditional statements.py
@@ -0,0 +1,46 @@
+# name="julia"
+# num=8
+
+# if name=="potato" or num ==7:
+#     print("Potato")
+# elif name=="Emmanuel":
+#     print("Emmanuel")
+# elif name =="Julia":
+#     print("julia")
+# else:
+#     print("none of those statements")
+# 
+# Loops
+# Repeating a block of code
+# Two types
+# 1.For Loops
+# 2.While loops
+
+# For Loops
+# power outage in Kilifi
+
+# Kilifi =["Magarini", "Shakahola", "Madunguni",] #no power for these counties
+
+# for constituencies in Kilifi:
+#  print("no power for:",constituencies)
+#  print(constituencies)
+
+# #  For loops:using range
+
+welcome_message="Welcome to PLP"
+for i in range(6):
+ 1,2,3,4,5,6
+ print (welcome_message)
+
+#  assignemt,for loop that has a start and a stop for the range
+# Example with 2 arguments
+for e in range(-1, 7):
+ print(e, end=", ")
+
+#  Example with 3 arguments
+for e in range(-2, 5, 3):
+ print(e, end=", ")
+
+#  Example with 1 argument
+for e in range(5):
+ print(e, end=", ")
--- dict.py
+++
@@ -1,3 +0,0 @@
-my_dict={"a": 1, "b": 2, "c": 3}
-result=my_dict["b"]
-print(result)
