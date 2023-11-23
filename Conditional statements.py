d35d4d6a2e285dde4fe2cae254d988d56d4fd750
Bigboss <ochie@Bigboss>
2023-11-23 23:06:14 +0300

    functions

--- Conditional statements.py
+++ Conditional statements.py
@@ -1,46 +1,91 @@
-# name="julia"
-# num=8
+# 1. if statement
+# 2.if...else statement
+# 3. if...elif...else statement
 
-# if name=="potato" or num ==7:
-#     print("Potato")
-# elif name=="Emmanuel":
-#     print("Emmanuel")
-# elif name =="Julia":
-#     print("julia")
+# if statement
+
+# relationship=False
+
+# if relationship: # condition
+#    # body
+#    print("Taken")
+
+
+# if else statement
+# loyal_relationship=False
+
+# if loyal_relationship: # condition
+#    # body
+#    print("Give your girlfriend your phone")
+
+# else: # condition
+#    # body
+#    print("You walk in her high heels and mummy jeans to work")
+
+
+# num = 2
+# if num >= 20:
+#     print("Number Greater or Equal to 20")
 # else:
-#     print("none of those statements")
-# 
-# Loops
-# Repeating a block of code
-# Two types
-# 1.For Loops
-# 2.While loops
+#     print("The Number is less than 20")
 
-# For Loops
-# power outage in Kilifi
+# #if elif else
+# fuliza=True
+# okoa=True
 
-# Kilifi =["Magarini", "Shakahola", "Madunguni",] #no power for these counties
+# if fuliza: # conditions
+#     # body
+#     print("Usiweke kwa hii number")
 
-# for constituencies in Kilifi:
-#  print("no power for:",constituencies)
-#  print(constituencies)
+# elif okoa:
+#    print("Reverse call")
 
-# #  For loops:using range
+# else:
+#    print("Maisha London,soft life")
 
-welcome_message="Welcome to PLP"
-for i in range(6):
- 1,2,3,4,5,6
- print (welcome_message)
+# comment
+
+# marks=90
+# if (marks >= 90 and marks <= 100): # 90-100
+#    print("A")
+# elif marks >= 80: # else if 80-90
+#    print("8")
+# elif marks >=70:
+#    print("Fail")
+
+
+#  Loops
+#  Repeating a block of code
+
+#  Two types
+#  1.For Loops
+#  2.While loops
+
+#  For Loops
+#  power outage in Kilifi
+
+#  Kilifi =["Magarini", "Shakahola", "Madunguni",] #no power for these counties
+
+#  for constituencies in Kilifi:
+#   print("no power for:",constituencies)
+#   print(constituencies)
+
+#    For loops:using range
+
+# welcome_message="Welcome to PLP"
+# for i in range(6):
+#  1,2,3,4,5,6
+#  print (welcome_message)
 
 #  assignemt,for loop that has a start and a stop for the range
-# Example with 2 arguments
-for e in range(-1, 7):
- print(e, end=", ")
+#  Example with 2 arguments
+# for e in range(-1, 7):
+#  print(e, end=", ")
 
-#  Example with 3 arguments
-for e in range(-2, 5, 3):
- print(e, end=", ")
+#   Example with 3 arguments
+# for e in range(-2, 5, 3):
+#  print(e, end=", ")
 
-#  Example with 1 argument
-for e in range(5):
- print(e, end=", ")
+#   Example with 1 argument
+# for e in range(5):
+#  print(e, end=", ")
---
+++ Functions/first_code.py
@@ -0,0 +1,39 @@
+# def sum():
+#print(2 + 3) #this is the body of the function
+
+# sum() # calling the function
+
+#     # Functions
+#      1. def to initiate/define a function
+#      for example:
+    
+#      def <function-name()>:
+#      def kilifi_function():
+
+#  2. identation:
+#  def kilifi_function():
+#  <---->This is identation,and your code should be here
+#        # This is also the body of the function
+#        # result
+
+#  # 3. Calling the function
+#  # Outside the function, type the function name, with its curly braces
+
+
+
+# Assignments:
+#1. Check on the naming convection for the function names
+
+# 1. variables and fields name starts with a Lower-Case and every second word first letter Upper-Case
+# Examples
+# Second_Name
+# firstName;
+
+# 2.Class names start with Upper-Case letter and every second word's first letter also Upper-Case
+# Example
+# class People{
+# }
+
+# All libraries,packages,directories and source files will be named LowerCase with Underscores
+# Example
+# const pi=3.14 -> const_pi=3.14
